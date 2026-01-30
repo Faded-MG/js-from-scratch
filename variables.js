10ad62ce3fe36ad72e2ef254cd7d2f1fe0f67f9c
FADED <mahletgetinet17@gmail.com>
2026-01-30 18:35:21 +0300

    js-from-scratch

---
+++ variables.js
@@ -0,0 +1,20 @@
+// variable= A container that stores a value.
+          // Behaves as if it were the value it contains.
+// 1. declaration    let x;
+// 2. assignment      x= 200;
+
+//let age = 25;
+//let price = 10.99;
+//let gpa = 2.1;
+//
+// console.log(`You are ${age} years old.`);
+// console.log(`The price is $ ${price}.`);
+//  console.log(`Your gpa is: ${gpa}`);
+
+let fullName = "Faded";
+let age = 20;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}.`;
+document.getElementById("p2").textContent = `You are ${age} years old.`;
+document.getElementById("p3").textContent = `Enrolled  ${isStudent}.`;
